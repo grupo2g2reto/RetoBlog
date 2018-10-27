f530e828c527d1f86c4d806bc215e4213cfc0037
jonandonicastelo <jonandonicasteloinf@outlook.com>
2018-10-27 20:31:31 +0200

    Pagina mostrar comentarios añadida

--- blogg2/mostrarEntrada.php
+++ blogg2/mostrarEntrada.php
@@ -1,6 +1,4 @@
-
 <?php
-	session_start();
 	//LLAMAMOS A LA CABECERA
 	require_once('cabecera.html');
 
@@ -11,10 +9,16 @@
 	// $sentencia=$db->prepare("SELECT * FROM entrada e, comentario c WHERE e.titulo=c.titulo_entrada"); 
 	// $sentencia->execute();
 
-	$sentencia=$db->prepare("SELECT titulo 'et' FROM entrada"); 
+	if(isset($_GET['var']) && $_GET['var'] !== ''){
+		$titulo = $_GET['var'];
+	  } else {
+		echo "failed";
+	  }
+
+	$sentencia=$db->prepare("SELECT entrada.titulo 'et' , entrada.contenido 'ec', entrada.fecha_entrada 'ef', comentario.contenido 'cc', comentario.fecha 'cf' FROM entrada LEFT JOIN comentario ON entrada.titulo=comentario.titulo_entrada WHERE entrada.titulo='$titulo'"); 
 	$sentencia->execute();
 
-
+	
 		//LLAMAMOS AL MENU DEPENDIENDO DEL USUARIO REGISTRADO
 		if (isset($usuario)){
 			if ($usuario=='admin'){
@@ -27,11 +31,10 @@
 		}
 		echo "<div>
 		<section>";
-		echo '<h2>Entradas:</h2><br>';
-	foreach($sentencia as $entrada){	
-		$variable=$entrada["et"];	
-	    echo '<article id="entrada"><br><a href="mostrarEntrada.php?var='.$variable.'">'.$variable.'</a>';
-	}
+		echo '<h2>'.$titulo.'</h2><br>';
+		foreach($sentencia as $entrada){		
+			echo '<article><br><h3>Contenido:</h3><br><p>'.$entrada['ec'].'</p><br><h3>Fecha de entrada:</h3><br><p>'.$entrada['ef'].'</p>';
+		}
 	} catch(PDOException $e) {
 	  echo 'Error: ' . $e->getMessage();
 	} 
